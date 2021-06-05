9c42be612c5dc2c6776c9ff711ad91475ee7853b
izadora-oliveira <74382460+izadora-oliveira@users.noreply.github.com>
2021-06-05 17:34:40 -0300

    att

---
+++ 8_null_undefined/js/scripts.js
@@ -0,0 +1,11 @@
+// Hoisting - içamento
+console.log(sobrenome);
+
+
+var nome = null;
+var sobrenome = "batist";
+
+console.log(nome);
+console.log(sobrenome);
+nome = "matheus";
+console.log(nome);
---
+++ 9_chave_valor/js/scripts.js
@@ -0,0 +1,24 @@
+var obj = {
+    nome: "matheus",
+    idade: 29,
+    profissao: "programador",
+    estatrabalhando: true,
+};
+
+console.log(obj);
+console.log(typeof obj);
+
+
+console.log(obj.nome);
+
+console.log(obj.idade);
+
+console.log(obj.profissao);
+
+console.log("o meu nome é: " + obj.nome);
+obj.nome = "pedro";
+console.log(obj.nome);
+console.log(obj);
+obj.graduacao = true;
+
+console.log(obj);
