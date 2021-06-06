379b8741110304bf815aeef375f6da8be5543d11
izadora-oliveira <74382460+izadora-oliveira@users.noreply.github.com>
2021-06-06 19:15:18 -0300

    att

---
+++ 11_ifelse/js/scripts.js
@@ -0,0 +1,12 @@
+var idade = 16;
+
+console.log("antes do if");
+
+if (idade > 18) {
+    console.log("pode tirar carteira")
+}
+console.log("depois do if");
+
+if (idade > 15) {
+    console.log("precisa esperar 3 anos ainda")
+}
