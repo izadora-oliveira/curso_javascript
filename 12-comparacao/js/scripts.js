785e32446207bbdc593cff39cdc11750eea8e82c
izadora-oliveira <74382460+izadora-oliveira@users.noreply.github.com>
2021-06-07 18:38:16 -0300

    att

---
+++ 12-comparacao/js/scripts.js
@@ -0,0 +1,39 @@
+var idade = 18;
+var possuicarro = 1;
+
+if (idade >= 18) {
+    console.log("pode fazer carteira")
+}
+
+if (idade <= 17) {
+    console.log("não pode fazer carteira")
+}
+
+if (possuicarro) {
+    console.log("o usuario já pode andar de carro")
+}
+
+var nome = "izadora";
+
+if (nome == "izadora"){
+    console.log("seu nome é izadora")
+
+}
+
+if (nome != "izadora"){
+    console.log("o nome não é izadora")
+
+}
+
+if (30 > 5 ){
+    console.log("passou")
+
+}
+
+if (10<20){
+    console.log("passou 2")
+
+}
+
+
+
