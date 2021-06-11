53aa1cf56a0fe2780521d53b5c30060dc5fc9efb
izadora-oliveira <74382460+izadora-oliveira@users.noreply.github.com>
2021-06-11 18:43:47 -0300

    11/06

---
+++ 13-else_ifelse/js/scripts.js
@@ -0,0 +1,11 @@
+var nome = "izadora";
+
+if(nome == "pedro") {
+    console.log("o nome dele é pedro");
+
+} else if (nome == "izadora"){
+    console.log("o nome é izadora");
+
+}else {
+    console.log("ele possui outro nome");
+}
