43497652c7446b0ea0218f82140b0f9dd2fe2634
izadora-oliveira <74382460+izadora-oliveira@users.noreply.github.com>
2021-06-16 16:26:46 -0300

    16/06

---
+++ 15-&&/js/scripts.js
@@ -0,0 +1,9 @@
+var idade = 16;
+var nome = "izadora";
+
+if(nome == "izadora" && idade == 16){
+    console.log("izadora pode entrar na aula de esgrima");
+}else {
+    console.log("esta não é izadora");
+}
+
---
+++ 16-OR/js/scripts.js
@@ -0,0 +1,10 @@
+idade = 18;
+nome = "izadora";
+
+console.log("hello")
+
+if (nome == "izadora" || idade > 16 ) {
+    console.log("pode entrar na aula de esgrima ")
+}else {
+    console.log("não pode entrar")
+}
