f8c9a75e13ed15c0bf57ec05706059e2a4817e35
izadora-oliveira <74382460+izadora-oliveira@users.noreply.github.com>
2021-06-19 16:44:47 -0300

    19/06

---
+++ 18-while/js/scripts.js
@@ -0,0 +1,17 @@
+var x = 0;
+
+
+while (x < 5){
+    console.log("repetir vezes: " + x);
+    //incrementador
+    x++;
+
+}
+
+var arr = ['a','b','c','d'];
+var y = 0;
+
+while (y <= 3 ) {
+    console.log(arr[y]);
+    y++;
+}
