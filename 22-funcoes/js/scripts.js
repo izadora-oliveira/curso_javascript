93509a7fa11570af3ac9b90aa444788e614db1d1
izadora-oliveira <74382460+izadora-oliveira@users.noreply.github.com>
2021-06-24 18:51:05 -0300

    24/06

---
+++ 22-funcoes/js/scripts.js
@@ -0,0 +1,33 @@
+function primeiraFuncao(){
+    console.log("hello world das funções");
+
+}
+
+primeiraFuncao();
+
+function dizerNome(nome){
+    console.log("o nome é " + nome);
+
+}
+
+dizerNome("izadora");
+
+var nomeDoBancoDeDados = "izadora";
+
+dizerNome(nomeDoBancoDeDados);
+
+function soma(a,b){
+    var soma = a+b;
+    return soma;
+}
+
+var somaUm = soma(2,5);
+
+console.log(somaUm);
+
+var somDois = soma(5,5);
+console.log(somDois);
+console.log(soma(6,5));
+
+
+
