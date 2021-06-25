a78b402f0f081be547b6fe22fd5bc286041ed641
izadora-oliveira <74382460+izadora-oliveira@users.noreply.github.com>
2021-06-25 18:40:27 -0300

    25/06

---
+++ 25-let_e_const/js/scripts.js
@@ -0,0 +1,20 @@
+let x = 5; // var x = 5;
+
+const y = 10;
+
+x = 12;
+
+console.log(x);
+console.log("const " + y);
+
+if (true){
+    let x = 20;
+    const y = 50;
+    
+    
+    console.log("const if " + y); 
+    console.log(x);
+}
+
+
+console.log(x);
